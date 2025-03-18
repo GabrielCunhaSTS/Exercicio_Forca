92964f2b89618c7ef78b77ad8efad6116edd0442
Gabriel <ggabrielcunha06@gmail.com>
2025-03-18 18:35:02 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string palavraSecreta;
+char letra;
+bool existeNaPalavra;
+
+Console.WriteLine("--- Forca ---\n");
+
+Console.Write("Qual a palavra secreta? ");
+palavraSecreta = Console.ReadLine()!.Trim().ToLower();
+
+Console.Write("Qual a letra? ");
+letra = char.ToLower(Console.ReadKey().KeyChar);
+
+Console.WriteLine("\n");
+
+existeNaPalavra = palavraSecreta.Contains(letra);
+
+Console.WriteLine($"A letra \"{letra}\" existe na palavra secreta => {existeNaPalavra}");
+
